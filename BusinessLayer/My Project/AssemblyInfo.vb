ff62c06d00d3de250388dbaa9d209a3444521ed4
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2022-09-26 13:07:01 -0300

    Add files via upload

---
+++ BusinessLayer/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La informaci�n general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos atributos para modificar la informaci�n
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("BusinessLayer")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Genesys")> 
+<Assembly: AssemblyProduct("BusinessLayer")> 
+<Assembly: AssemblyCopyright("Copyright � Genesys 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+<Assembly: Guid("695646fb-6c0c-4a44-a69f-c89922022a08")> 
+
+' La informaci�n de versi�n de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versi�n principal
+'      Versi�n secundaria 
+'      N�mero de compilaci�n
+'      Revisi�n
+'
+' Puede especificar todos los valores o usar los valores predeterminados de n�mero de compilaci�n y de revisi�n 
+' mediante el asterisco ('*'), como se muestra a continuaci�n:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
